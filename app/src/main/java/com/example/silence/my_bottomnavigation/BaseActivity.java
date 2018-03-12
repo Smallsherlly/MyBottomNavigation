b269808bf5db38e8a94163b5cab25cf4580a5e40
silence <779854387@qq.com>
2018-03-12 15:31:13 +0800

    添加部分控件

--- app/src/main/java/com/example/silence/my_bottomnavigation/BaseActivity.java
+++ app/src/main/java/com/example/silence/my_bottomnavigation/BaseActivity.java
@@ -1,11 +1,24 @@
 package com.example.silence.my_bottomnavigation;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
 /**
  * Created by Silence on 2018/3/12.
  */
 
 public class BaseActivity extends AppCompatActivity {
-
+    private ViewPager mViewPager;
+    private BottomNavigation mBottomNavigation;
+    public ViewPager getViewPager() {
+        return mViewPager;
+    }
+    public BottomNavigation getBottomNavigation() {
+        if (null == mBottomNavigation) {
+            mBottomNavigation = (BottomNavigation) findViewById(R.id.BottomNavigation);
+        }
+        return mBottomNavigation;
+    }
 }
--- app/src/main/java/com/example/silence/my_bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/silence/my_bottomnavigation/MainActivity.java
@@ -10,9 +10,19 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final ViewGroup root = (ViewGroup) findViewById(R.id.CoordinatorLayout01);
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_main);// 设置布局文件
+        final ViewGroup root = (ViewGroup) findViewById(R.id.CoordinatorLayout01);// 获取协调布局
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);// 获取工具栏控件
         setSupportActionBar(toolbar);// 用ToolBar替代原本的ActionBar
+
+        initializeBottomNavigation(savedInstanceState);// 初始化底部导航栏
+        initializeUI(savedInstanceState);// 初始化界面
+
+    }
+
+    private void initializeUI(Bundle savedInstanceState) {
+    }
+
+    private void initializeBottomNavigation(Bundle savedInstanceState) {
     }
 }
