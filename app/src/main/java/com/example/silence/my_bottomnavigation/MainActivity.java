8aa6d65e5ecca0507c1afe39faa6239ca7dd978c
silence <779854387@qq.com>
2018-03-14 11:12:06 +0800

    添加部分类和布局

--- app/src/main/java/com/example/silence/my_bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/silence/my_bottomnavigation/MainActivity.java
@@ -1,31 +1,52 @@
 package com.example.silence.my_bottomnavigation;
 
+import android.support.annotation.IdRes;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import it.sephiroth.android.library.bottomnavigation.BadgeProvider;
 import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 import it.sephiroth.android.library.bottomnavigation.FloatingActionButtonBehavior;
 
-public class MainActivity extends BaseActivity {
+import static android.util.Log.INFO;
+import static it.sephiroth.android.library.bottomnavigation.MiscUtils.log;
+
+public class MainActivity extends BaseActivity implements BottomNavigation.OnMenuItemSelectionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);// 设置布局文件
-//        final ViewGroup root = (ViewGroup) findViewById(R.id.CoordinatorLayout01);// 获取协调布局
-//        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);// 获取工具栏控件
-//        setSupportActionBar(toolbar);// 用ToolBar替代原本的ActionBar
-//
-//        initializeBottomNavigation(savedInstanceState);// 初始化底部导航栏
-//        initializeUI(savedInstanceState);// 初始化界面
+        final ViewGroup root = (ViewGroup) findViewById(R.id.CoordinatorLayout01);// 获取协调布局
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);// 获取工具栏控件
+        setSupportActionBar(toolbar);// 用ToolBar替代原本的ActionBar
+
+        final boolean translucentNavigation = hasTranslucentNavigation();// 设置导航栏隐藏？待确认
+
+        if (translucentNavigation) {
+            final ViewPager viewPager = getViewPager();
+            if (null != viewPager) {
+                ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) viewPager.getLayoutParams();
+                params.bottomMargin = -getNavigationBarHeight();
+            }
+        }
+
+       initializeBottomNavigation(savedInstanceState);// 初始化底部导航栏
+       initializeUI(savedInstanceState);// 初始化界面
 
     }
 
@@ -39,7 +60,7 @@ public class MainActivity extends BaseActivity {
                 @Override
                 public void onMenuChanged(final BottomNavigation parent) {
 
-                    //viewPager.setAdapter(new ViewPagerAdapter(MainActivity.this, parent.getMenuItemCount()));
+                    viewPager.setAdapter(new ViewPagerAdapter(MainActivity.this, parent.getMenuItemCount()));
                     viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
                         @Override
                         public void onPageScrolled(
@@ -64,9 +85,52 @@ public class MainActivity extends BaseActivity {
     private void initializeBottomNavigation(Bundle savedInstanceState) {
         if (null == savedInstanceState) {
             getBottomNavigation().setDefaultSelectedIndex(0);
-            final BadgeProvider provider = getBottomNavigation().getBadgeProvider();
-            provider.show(R.id.bbn_item3);
-            provider.show(R.id.bbn_item2);
+        }
+    }
+
+    @Override
+    public void onMenuItemSelect(final int itemId, final int position, final boolean fromUser) {
+     //   log(TAG, INFO, "onMenuItemSelect(" + itemId + ", " + position + ", " + fromUser + ")");
+        if (fromUser) {
+            getBottomNavigation().getBadgeProvider().remove(itemId);
+            if (null != getViewPager()) {
+                getViewPager().setCurrentItem(position);
+            }
+        }
+    }
+
+    @Override
+    public void onMenuItemReselect(@IdRes final int itemId, final int position, final boolean fromUser) {
+      //  log(TAG, INFO, "onMenuItemReselect(" + itemId + ", " + position + ", " + fromUser + ")");
+
+        if (fromUser) {
+            final FragmentManager manager = getSupportFragmentManager();
+            MainActivityFragment fragment = (MainActivityFragment) manager.findFragmentById(R.id.fragment);
+            if (null != fragment) {
+                fragment.scrollToTop();
+            }
+        }
+
+    }
+
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final int mCount;
+
+        public ViewPagerAdapter(final AppCompatActivity activity, int count) {
+            super(activity.getSupportFragmentManager());
+            this.mCount = count;
+        }
+
+        @Override
+        public Fragment getItem(final int position) {
+            return new MainActivityFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return mCount;
         }
     }
 }
